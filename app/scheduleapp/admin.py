8b9c2ac468e5e53357dfd01087e84c9d310722e9
Rachel Marsden <ram0679@yahoo.com>
2023-07-09 15:44:05 -0400

    add tests

--- app/scheduleapp/admin.py
+++ app/scheduleapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Teacher, Schedule, Activity
 
-# Register your models here.
+admin.site.register(Teacher)
+admin.site.register(Schedule)
+admin.site.register(Activity)
