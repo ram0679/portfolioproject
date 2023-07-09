73fba08130bc2e5e61c00a6e0b6f13c03ae17ea8
Rachel Marsden <ram0679@yahoo.com>
2023-07-09 15:35:37 -0400

    add tests

--- app/scheduleapp/models.py
+++ app/scheduleapp/models.py
@@ -2,11 +2,7 @@ from django.db import models
 
 class Teacher(models.Model):
     id = models.CharField(primary_key=True, max_length=20)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-    email = models.EmailField(unique=True)
-    grade = models.CharField(max_length=1)
-    subject = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
 
 class Schedule(models.Model):
     id = models.CharField(primary_key=True, max_length=20)
--- app/scheduleapp/tests.py
+++ app/scheduleapp/tests.py
@@ -1,3 +1,31 @@
 from django.test import TestCase
+import pytest
+from scheduleapp.models import Teacher
 
-# Create your tests here.
+
+@pytest.fixture
+def new_teacher(db):
+    teacher = Teacher.objects.create(
+        name='First_test_teacher',
+    )
+    return teacher
+
+
+def test_search_teachers(new_teacher):
+    assert Teacher.objects.filter(name='First_test_teacher').exists()
+
+def test_update_teacher(new_teacher):
+    new_teacher.name = 'First_test_teacher_renamed'
+    new_teacher.save()
+    assert Teacher.objects.filter(name='First_test_teacher_renamed').exists()
+
+
+@pytest.fixture
+def another_teacher(db):
+    teacher = Teacher.objects.create(
+        name = 'Second_test_teacher',
+    )
+    return teacher
+
+def test_compare_teachers(new_teacher, another_teacher):
+    assert new_teacher.pk != another_teacher.pk
